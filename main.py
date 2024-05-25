7879c3c7325af632af04559e178c1b1b84360131
Kalpesh <kalpesh2003patel@gmail.com>
2024-05-25 10:22:26 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from ChurnPredictor import logger
+from ChurnPredictor.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
